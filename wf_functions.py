d1f6583ef0e9a0610b733a97eccc91624cc30117
Donald McClymont <Donaldm2020@gmail.com>
2023-04-23 01:11:24 +0100

    hopefully mainly just pygments and hladapter to get sorted to get something being produced

--- wf_functions.py
+++ wf_functions.py
@@ -1,6 +1,10 @@
 #This is sort of a lift of the web2py book to make at least somewhat compatible with py4web and
 #get most of my markmin files surfaced as html on py4web
 
+from pydal._compat import to_native, to_bytes, to_unicode
+from pydal.validators import *
+
+
 def splitter(x):
     a, b = x.split(':', 1)
     return a.strip(), b.strip()
